c0596eeb67709805ea5d051f4a8c9a6f3ded710b
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-08-03 18:20:57 -0500

    initial setup with baseline code

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import Work from './pages/Work';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+  
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'Work') {
+      return <Work />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
